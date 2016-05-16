825c08a86e6eeedac32a3385fc72996cb20990c9
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-05-16 23:00:01 +0700

    Add rules for date (js)

---
+++ lib/Data/Sah/Coerce/js/date/obj_Date.pm
@@ -0,0 +1,42 @@
+package Data::Sah::Coerce::js::date::obj_Date;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        enable_by_default => 1,
+        might_die => 1, # we throw exception date is invalid
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{expr_match} = join(
+        " && ",
+        "($dt instanceof Date)",
+    );
+
+    $res->{expr_coerce} = "isNaN($dt) ? (throw new Error('Invalid date')) : $dt";
+
+    $res;
+}
+
+1;
+# ABSTRACT: Coerce date from Date object
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
+
+This is basically just to throw an error when date is invalid.
+0;115;0c
---
+++ lib/Data/Sah/Coerce/js/date/str.pm
@@ -0,0 +1,43 @@
+package Data::Sah::Coerce::js::date::str;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        enable_by_default => 1,
+        might_die => 1, # we throw exception date is invalid
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{expr_match} = join(
+        " && ",
+        "typeof($dt)=='string'",
+    );
+
+    # note: (function(a,b,c){...})() is a trick to simulate lexical variables
+    $res->{expr_coerce} = "(function (_m) { _m = new Date($dt); if (isNaN(_m)) throw new Error('Invalid date'); return _m })()";
+
+    $res;
+}
+
+1;
+# ABSTRACT: Coerce date from string
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
+
+This will simply use JavaScript's C<Date.parse()>, but will throw an error when
+date is invalid.
