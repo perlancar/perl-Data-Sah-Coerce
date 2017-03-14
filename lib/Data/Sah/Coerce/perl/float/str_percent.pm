7417e12eba850ece6cfad06a7d70d09d62fa8f44
perlancar (on pc-office) <perlancar@gmail.com>
2017-03-14 19:39:57 +0700

    Add coerce rules: {int,float}::str_percent

---
+++ lib/Data/Sah/Coerce/perl/float/str_percent.pm
@@ -0,0 +1,39 @@
+package Data::Sah::Coerce::perl::float::str_percent;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 2,
+        enable_by_default => 0,
+        prio => 50,
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{expr_match} = join(
+        " && ",
+        "$dt =~ /\\A([+-]?\\d+(?:\\.\\d*)?)%\\z/",
+    );
+
+    $res->{expr_coerce} = "\$1/100";
+    $res;
+}
+
+1;
+# ABSTRACT: Coerce float from percent string (e.g. "100.5%")
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
