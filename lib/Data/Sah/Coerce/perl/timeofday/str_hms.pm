e774b41f921420e723acfba3ce86d97a33d2c1c9
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-12-16 23:17:55 +0700

    Tweak timeofday str_hms regex: allow hh:mm in addition to hh:mm:ss

--- lib/Data/Sah/Coerce/perl/timeofday/str_hms.pm
+++ lib/Data/Sah/Coerce/perl/timeofday/str_hms.pm
@@ -26,18 +26,18 @@ sub coerce {
 
     $res->{expr_match} = join(
         " && ",
-        "$dt =~ /\\A([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})(\.[0-9]{1,9})?\\z/",
+        "$dt =~ /\\A([0-9]{1,2}):([0-9]{1,2})(?::([0-9]{1,2})(\.[0-9]{1,9})?)?\\z/",
     );
 
-    my $code_check = qq(if (\$1 > 23) { ["Invalid hour '\$1', must be between 0-23"] } elsif (\$2 > 59) { ["Invalid minute '\$2', must be between 0-59"] } elsif (\$3 > 59) { ["Invalid second '\$3', must be between 0-59"] });
+    my $code_check = qq(if (\$1 > 23) { ["Invalid hour '\$1', must be between 0-23"] } elsif (\$2 > 59) { ["Invalid minute '\$2', must be between 0-59"] } elsif (defined \$3 && \$3 > 59) { ["Invalid second '\$3', must be between 0-59"] });
 
     if ($coerce_to eq 'float') {
-        $res->{expr_coerce} = qq(do { $code_check else { [undef, \$1*3600 + \$2*60 + \$3 + (defined \$4 ? \$4 : 0)] } });
+        $res->{expr_coerce} = qq(do { $code_check else { [undef, \$1*3600 + \$2*60 + (defined \$3 ? \$3 : 0) + (defined \$4 ? \$4 : 0)] } });
     } elsif ($coerce_to eq 'str_hms') {
-        $res->{expr_coerce} = qq(do { $code_check else { [undef, defined(\$4) && \$4 > 0 ? sprintf("%02d:%02d:%s%.11g", \$1, \$2, (\$3 < 10 ? "0":""), \$3+\$4) : sprintf("%02d:%02d:%02d", \$1, \$2, \$3)] } });
+        $res->{expr_coerce} = qq(do { $code_check else { [undef, defined(\$4) && \$4 > 0 ? sprintf("%02d:%02d:%s%.11g", \$1, \$2, (!defined(\$3) || \$3 < 10 ? "0":""), (defined \$3 ? \$3:0)+\$4) : sprintf("%02d:%02d:%02d", \$1, \$2, (defined \$3 ? \$3 : 0))] } });
     } elsif ($coerce_to eq 'Date::TimeOfDay') {
         $res->{modules}{"Date::TimeOfDay"} //= 0.002;
-        $res->{expr_coerce} = qq([undef, Date::TimeOfDay->new(hour=>\$1, minute=>\$2, second=>\$3, nanosecond=>(defined \$4 ? \$4*1e9 : 0))]);
+        $res->{expr_coerce} = qq([undef, Date::TimeOfDay->new(hour=>\$1, minute=>\$2, second=>(defined \$3 ? \$3 : 0), nanosecond=>(defined \$4 ? \$4*1e9 : 0))]);
     } else {
         die "BUG: Unknown coerce_to value '$coerce_to', ".
             "please use float, str_hms, or Date::TimeOfDay";
