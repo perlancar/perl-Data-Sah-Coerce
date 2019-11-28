526c3521b92e88d9cbf213ccaad6dbf4cff77ba6
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-11-28 21:06:35 +0700

    Observe PERL_DATA_SAH_COERCE_SUPPORT_OLD_PREFIX

--- lib/Data/Sah/CoerceCommon.pm
+++ lib/Data/Sah/CoerceCommon.pm
@@ -7,6 +7,8 @@ package Data::Sah::CoerceCommon;
 use 5.010001;
 use strict 'subs', 'vars';
 
+our $SUPPORT_OLD_PREFIX = $ENV{PERL_DATA_SAH_COERCE_SUPPORT_OLD_PREFIX};
+
 our %Default_Rules = (
     perl => {
         bool       => [qw//],
@@ -142,7 +144,7 @@ sub get_coerce_rules {
     my @rule_names = @{ $Default_Rules{$compiler}{$typen} || [] };
     for my $item (@{ $args{coerce_rules} // [] }) {
         my $is_exclude = $item =~ s/\A!//;
-        if ($item =~ /\A\w+\z/) {
+        if ($SUPPORT_OLD_PREFIX && $item =~ /\A\w+\z/) {
             # old name
         } elsif ($item =~ /\AFrom_[A-Za-z0-9_]+::[A-Za-z0-9_]+\z/) {
             # new name
@@ -159,7 +161,7 @@ sub get_coerce_rules {
 
     my @rules;
     for my $rule_name (@rule_names) {
-        my $is_old_name = $rule_name =~ /\A\w+\z/;
+        my $is_old_name = $SUPPORT_OLD_PREFIX && $rule_name =~ /\A\w+\z/;
         my $mod = ($is_old_name ? $old_prefix : $prefix) . $rule_name;
         (my $mod_pm = "$mod.pm") =~ s!::!/!g;
         require $mod_pm;
@@ -216,3 +218,10 @@ sub get_coerce_rules {
 
 1;
 # ABSTRACT: Common stuffs for Data::Sah::Coerce and Data::Sah::CoerceJS
+
+=head1 ENVIRONMENT
+
+=head2 PERL_DATA_SAH_COERCE_SUPPORT_OLD_PREFIX
+
+If set to false, will not support old prefix
+(Data::Sah::Coerce::<$TARGET_TYPE>::<$SOURCE_TYPE_AND_DESC>. Mainly for testing.
